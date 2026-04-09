5dac15e30e42225b9ea2aa33a6e56690c38471b4
Chirag Gaonkar <55709751+ChiragGaonkar@users.noreply.github.com>
2026-04-09 16:35:24 +0000

    Remove index specification from primary key constraint

--- SOFTWARECOMPANY/Tables/EMPLOYEE.sql
+++ SOFTWARECOMPANY/Tables/EMPLOYEE.sql
@@ -2,5 +2,5 @@ CREATE TABLE softwarecompany.employee (
   emp_id NUMBER NOT NULL,
   first_name VARCHAR2(50 BYTE),
   last_name VARCHAR2(50 BYTE),
-  CONSTRAINT pk_employee PRIMARY KEY (emp_id) USING INDEX softwarecompany.emp_index
+  CONSTRAINT pk_employee PRIMARY KEY (emp_id)
 );
