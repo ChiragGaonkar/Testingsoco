bf9d779ad57e944cac26469b558d0f3ebbddbd0b
Chirag Gaonkar <55709751+ChiragGaonkar@users.noreply.github.com>
2026-04-09 16:56:58 +0000

    Change primary key index for papapedro table

--- SOFTWARECOMPANY/Tables/PAPAPEDRO.sql
+++ SOFTWARECOMPANY/Tables/PAPAPEDRO.sql
@@ -2,5 +2,5 @@ CREATE TABLE softwarecompany.papapedro (
   emp_id NUMBER NOT NULL,
   first_name VARCHAR2(50 BYTE),
   last_name VARCHAR2(50 BYTE),
-  CONSTRAINT pk_papapedro PRIMARY KEY (emp_id) USING INDEX softwarecompany.pk_papapedro
+  CONSTRAINT pk_papapedro PRIMARY KEY (emp_id) USING INDEX softwarecompany.pk_papakipari
 );
